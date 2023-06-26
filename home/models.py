65c0ef253f77e19056bdf633350b53048f70b28a
Paramdeep Singh <=>
2023-06-26 12:57:21 +0530

    sfas

--- home/models.py
+++ home/models.py
@@ -11,3 +11,13 @@ class otp(models.Model):
 
     def __str__(self) -> str:
         return self.otp_no
+
+class Menu(models.Model):
+    item_name = models.CharField(max_length=100)
+    item_description = models.TextField()
+    item_price = models.IntegerField()
+    item_image = models.ImageField(upload_to="menu_items")
+
+
+    def __str__(self) -> str:
+        return self.item_name
